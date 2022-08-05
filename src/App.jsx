aaee7e0edf960289c33beab7e10a994b19750b72
MATEO <sanromanxp@gmail.com>
2022-08-05 15:58:51 -0500

    v.02

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from './app.module.css'
 import {Row, Col} from 'antd'
 import 'antd/dist/antd.css'
 import {Button, Form, Input, Radio} from 'antd';
+import axios from 'axios'
 import "@fontsource/bebas-neue"
 
 function App() {
@@ -10,15 +11,45 @@ function App() {
     const [form] = Form.useForm();
 
     const handleFinish = (e) => {
-        alert('Correo enviado con datos: ' + JSON.stringify(e))
+        alert('Información enviada con éxito')
         console.log(e)
+        /*
+        axios.post('https://api.mailjet.com/v3.1/send', {
+            auth: {
+                username: 'deb94ba7ca2730c0e3a5f0172a8ed9f7',
+                password: '98abfd00210decadec30cdc09121ea4'
+            },
+            data: {
+                "Messages":
+                    [
+                        {
+                            "From": {
+                                "Email": "sanromanxp@gmail.com",
+                                "Name": "Mateo"
+                            },
+                            "To": [
+                                {
+                                    "Email": "msanroman@uap.edu.pe",
+                                    "Name": "Mateo"
+                                }
+                            ],
+                            "Subject": "Nuevo contacto",
+                            "TextPart": "Nuevo contacto!",
+                            "HTMLPart": `<h3>Buenos días</h3>Un nuevo contacto con código ${e.codigo}, documento ·${e.dni}, teléfono ·${e.celular} y correo ${e.correo} ha ingresado sus datos.`,
+                            "CustomID": "AppGettingStartedTest"
+                        }
+                    ]
+            }
+        })
+
+         */
     }
-
-
+//deb94ba7ca2730c0e3a5f0172a8ed9f7
+//98abfd00210decadec30cdc09121ea49
     return (
         <div className={styles.component}>
             <div className={styles.banner}>
-                <img src="./assets/images/A.png"/>
+                <img src="./assets/images/A.png" style={{objectFit: 'cover', width: '100%'}}/>
                 <div className={styles.formcontainer}>
                     <Form
                         layout='vertical'
@@ -38,14 +69,15 @@ function App() {
                         <Form.Item name="correo">
                             <Input size='large' placeholder="Correo Personal"/>
                         </Form.Item>
-                        <Form.Item >
-                            <Button  htmlType="submit" size='large' block type="primary">ENVIAR</Button>
+                        <Form.Item>
+                            <Button htmlType="submit" size='large' block type="primary">ENVIAR</Button>
                         </Form.Item>
                     </Form>
                 </div>
             </div>
             <div className={styles.slip}>
                 <img src="./assets/images/B.png"/>
+                <div className={styles.bContainer}></div>
             </div>
             <div className={styles.modeA}>
                 <img src="./assets/images/C.png"/>
